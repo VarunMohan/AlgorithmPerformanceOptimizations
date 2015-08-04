44f54cf70baddfad215fa2fcf6677656cb6f2280
Varun Mohan <vmohan@mit.edu>
2015-08-04 13:44:21 -0700

    Added Some comments

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 AlgorithmPerformanceOptimizations
 ---------------------------------------------
+Project contains optimizations for various common algorithms that maximize Cache Locality, minimize Branch Mispredication, and take advantage of CPU architecture
+
+Dependencies
+-------------------------------
+<code>brew install gnuzip --with-qt</code>
--- search/binary_search/binary_search_linear.cc
+++ search/binary_search/binary_search_linear.cc
@@ -3,7 +3,7 @@
 int binary_search_linear(int *arr, int target, int n_elements) {
     int low=0, high=n_elements-1;
     while (low <= high) {
-    if ((high - low <= 16)) { 
+    if ((high - low <= 16)) { // 16 * 4 bytes is size of L1 Cache-Line  
     	int v = linear_search(arr + low, target, high - low + 1);
     	if (v == -1) return -1;
     	return low + v;
