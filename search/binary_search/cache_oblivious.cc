2d4aa3bd0a281ec877c2faef9453f94f8412e090
vmohan@mit.edu <varunkmohan@gmail.com>
2015-08-04 00:26:15 -0700

    Optimization to cache_oblivious

--- search/binary_search/cache_oblivious.cc
+++ search/binary_search/cache_oblivious.cc
@@ -9,10 +9,10 @@ int binary_search_low_high(int *arr, int target, int n_elements, int low, int hi
 }
 
 int cache_oblivious(int *arr, int target, int n_elements) {
-    int start = 0;
+    if (*arr == target) return 0;
+    int start = 1;
     while (arr[start] < target) {
-	if (start == 0) start += 1;
-	else start *= 2;
+	start*=2;	
 	if (start > n_elements) return binary_search_low_high(arr, target, n_elements, start/2, n_elements-1);
     }
     return binary_search_low_high(arr, target, n_elements, start/2, start);
