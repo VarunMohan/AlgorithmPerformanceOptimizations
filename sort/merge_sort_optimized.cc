c5ec3ab4d795f79205dbae380ce175d1b6455b12
Max Lam <malam@linkedin.com>
2015-08-06 21:58:13 -0700

    <= 1

--- sort/merge_sort_optimized.cc
+++ sort/merge_sort_optimized.cc
@@ -18,19 +18,19 @@ inline void merge_optimized(int *C, int *A, int *B, int na, int nb) {
 
 inline void merge_sort_helper_optimized(int *sorted, int *start, int *end) {
     int n = start - end;
-    if (n == 1) {
+    if (n <= 1) {
       sorted[0] = *start;
       return;
     }
 
-	int *mid = start + n/2; 
+	int *mid = start + n/2;
     merge_sort_helper_optimized(sorted, start, mid);
     merge_sort_helper_optimized(sorted, mid, end);
     merge_optimized(sorted, start, mid, mid - start, end - mid);
   }
 
 inline void merge_sort_optimized(int *A, int n) {
-	int *B = new int[n]; 
+	int *B = new int[n];
 	merge_sort_helper_optimized(B, A, A+n);
 	for (int i = 0; i < n; ++i) {
 		A[i] = B[i];
