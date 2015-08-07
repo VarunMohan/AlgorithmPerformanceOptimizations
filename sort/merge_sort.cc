0a56c6299a86e2a56aa2565ba149e2c63907cb7a
Max Lam <malam@linkedin.com>
2015-08-06 20:29:26 -0700

    reordered functiosn in merge_sort

--- sort/merge_sort.cc
+++ sort/merge_sort.cc
@@ -1,7 +1,16 @@
-inline void merge_sort(int *A, int n) {
-	int *B = new int[n]; merge_sort_helper(B, A, n);
-	for (int i = 0; i < n; ++i) {
-		A[i] = B[i];
+inline void merge(int *C, int *A, int *B, int na, int nb) {
+	while (na > 0 && nb > 0) {
+    	if (*A <= *B) {
+			*C++ = *A++; na--;
+     	 } else {
+			*C++ = *B++; nb--;
+     	 }
+    }
+    while (na > 0) {
+      *C++ = *A++; na--;
+    }
+    while (nb >0) {
+      *C++ = *B++; nb--;
 	}
 }
 
@@ -16,18 +25,9 @@ inline void merge_sort_helper(int *B, int *A, int n) {
     merge(B, C, C + n/2, n/2, n - n/2);
   }
 
-inline void merge(int *C, int *A, int *B, int na, int nb) {
-	while (na > 0 && nb > 0) {
-    	if (*A <= *B) {
-			*C++ = *A++; na--;
-     	 } else {
-			*C++ = *B++; nb--;
-     	 }
-    }	
-    while (na > 0) {
-      *C++ = *A++; na--;
-    }
-    while (nb >0) {
-      *C++ = *B++; nb--;
+inline void merge_sort(int *A, int n) {
+	int *B = new int[n]; merge_sort_helper(B, A, n);
+	for (int i = 0; i < n; ++i) {
+		A[i] = B[i];
 	}
 }
