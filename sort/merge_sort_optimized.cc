1472dc7dccf5ae83f46e014fa2c4a7d63a6ed560
vmohan@mit.edu <varunkmohan@gmail.com>
2015-08-08 22:30:26 -0700

    Added quick sort optimized

--- sort/merge_sort_optimized.cc
+++ sort/merge_sort_optimized.cc
@@ -1,14 +1,4 @@
-#define CMP_SWAP(i, j) if (a[i] > a[j])			\
-	{ int tmp = a[i]; a[i] = a[j]; a[j] = tmp; }
-
-void sort8_network(int *a)
-{
-    CMP_SWAP(0, 1); CMP_SWAP(2, 3); CMP_SWAP(4, 5); CMP_SWAP(6, 7);
-    CMP_SWAP(0, 2); CMP_SWAP(1, 3); CMP_SWAP(4, 6); CMP_SWAP(5, 7);
-    CMP_SWAP(1, 2); CMP_SWAP(5, 6); CMP_SWAP(0, 4); CMP_SWAP(1, 5);
-    CMP_SWAP(2, 6); CMP_SWAP(3, 7); CMP_SWAP(2, 4); CMP_SWAP(3, 5);
-    CMP_SWAP(1, 2); CMP_SWAP(3, 4); CMP_SWAP(5, 6);
-}
+#include "sort_utils.cc"
 
 void sort_le_8(int *out, int *in, int n) {
     int aux[8];
--- sort/quick_sort.cc
+++ sort/quick_sort.cc
@@ -1,11 +1,43 @@
 #include <iostream>
+#include "sort_utils.cc" 
+
 void quick_sort(int *A, int n);
 void quick_sort_helper(int *start, int *end);
 int partition(int *start, int *end);
 void val_swap(int *x, int *y);
+void quick_sort_optimized(int *A, int n);
+void quick_sort_optimized_helper(int *start, int *end);
 
 void quick_sort(int *A, int n) {
 	quick_sort_helper(A, A+n-1);
+}
+
+void quick_sort_optimized(int *A, int n) {
+	int* B = new int[n+8];
+	for (int i = 0; i < n; i++) {
+		B[i] = A[i];
+	} 
+	for (int i = n; i < n + 9; i++) {
+		B[i] = INT_MAX;
+	}
+	quick_sort_optimized_helper(B, B+n-1);
+	for (int i = 0; i < n; i++) {
+		A[i] = B[i];
+	}
+}
+
+void quick_sort_optimized_helper(int *start, int *end) {
+	int n = end - start;
+	if (n == 0) {
+		return;
+	}
+	if (n < 8) {
+		sort8_network(start);
+		return;
+	} 
+	int p = partition(start, end);
+	quick_sort_helper(start, start + p - 1);
+	quick_sort_helper(start + p, end);
 }
 
 void quick_sort_helper(int *start, int *end) {
